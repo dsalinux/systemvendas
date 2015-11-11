8c9eb97c05ecbd28dbc896be8a7a1a730e7d70c6
ifnmg <ifnmg@ARI-IBI-COM-B27.ifnmgarinos.edu.br>
2015-11-11 09:00:28 -0200

    Modificação de Vendas: Euzimar, Romirés, Thifanny, Ríllary
    faltando: pagina xhtml

---
+++ src/main/java/br/edu/ifnmg/arinos/systemvendas/entidade/Venda.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.arinos.systemvendas.entidade;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *
+ * @author ifnmg
+ */
+public class Venda {
+    private Integer id;
+    private Date data;
+    private String observacao;
+    private float valor_total;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public float getValor_total() {
+        return valor_total;
+    }
+
+    public void setValor_total(float valor_total) {
+        this.valor_total = valor_total;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + this.id;
+        hash = 97 * hash + Objects.hashCode(this.data);
+        hash = 97 * hash + Objects.hashCode(this.observacao);
+        hash = 97 * hash + Float.floatToIntBits(this.valor_total);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Venda other = (Venda) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.data, other.data)) {
+            return false;
+        }
+        if (!Objects.equals(this.observacao, other.observacao)) {
+            return false;
+        }
+        if (Float.floatToIntBits(this.valor_total) != Float.floatToIntBits(other.valor_total)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
