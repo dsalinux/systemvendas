404cef6c3f4be32177fe3cf659e2cb6db13015f2
ifnmg <ifnmg@ARI-IBI-COM-B36.ifnmgarinos.edu.br>
2015-11-23 10:08:53 -0200

    alteração em cidade

--- src/main/java/br/edu/ifnmg/arinos/systemvendas/entidade/Cidade.java
+++ src/main/java/br/edu/ifnmg/arinos/systemvendas/entidade/Cidade.java
@@ -11,7 +11,7 @@ public class Cidade {
     private Integer id;
     private String nome;
     private String uf;
-    private String pais;
+    private String pais = "Brasil";
 
     public Integer getId() {
         return id;
